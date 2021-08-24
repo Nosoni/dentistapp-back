cb875a31151c9fd39502e7af312fcf576dd61d63
Franco <francisco.recalde.91@gmail.com>
2021-08-24 11:12:03 -0400

    avances en usuarios

--- src/controller/usuarios.js
+++ src/controller/usuarios.js
@@ -1,18 +1,50 @@
-var models = require("../models/inicializar_modelos").usuarios;
+const models = require("../models/inicializar_modelos").usuarios;
+const { Op } = require("sequelize")
+const bcrypt = require("bcrypt")
 
 module.exports = {
-  listar(_, res) {
-    return models.findAll({})
-      .then(result => res.status(200).send(result))
-      .catch(() => res.status(400).send("error"));
-  },
-  filtrar(req, res) {
-    return models.findAll({
+  async crear(req, res) {
+    const { usuario, password } = req.body;
+    const userExists = await models.findAll({
       where: {
-        usuario: req.params.usuario,
+        usuario
       }
     })
-      .then(result => res.status(200).send(result))
-      .catch(() => res.status(400).send("error"))
+
+    if (userExists.length > 0) {
+      return res.status(409).send("Ya existe dicho usuario.")
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+    const userCreate = await models.create({
+      usuario,
+      password: hash
+    })
+
+    return res.status(200).json(userCreate)
+  },
+  async listar(_, res) {
+    try {
+      const list = await models.findAll({});
+      return res.status(200).json(list)
+    } catch (error) {
+      return res.status(400).send("error")
+    }
+  },
+  async filtrar(req, res) {
+    try {
+      const { usuario } = req.params;
+      const usuariosFiltrados = await models.findAll({
+        where: {
+          usuario: {
+            [Op.substring]: usuario,
+          }
+        }
+      })
+
+      return res.status(200).json(usuariosFiltrados)
+    } catch (error) {
+      return res.status(400).send("error")
+    }
   },
 }
--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,6 +2,7 @@ const controller = require("../controller/usuarios")
 const servicio = "usuarios";
 
 module.exports = (app) => {
+  app.post(`/${servicio}/crear`, controller.crear)
   app.get(`/${servicio}/listar`, controller.listar)
   app.get(`/${servicio}/filtrar/:usuario`, controller.filtrar)
 };
