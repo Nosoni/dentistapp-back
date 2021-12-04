a27fd65a953506f90ceaf24aca50532cca563840
Franco <francisco.recalde.91@gmail.com>
2021-12-04 19:04:05 -0300

    deleted pruebas logs

--- src/controller/facturas.js
+++ src/controller/facturas.js
@@ -15,8 +15,6 @@ module.exports = {
       const { cabecera, detalle } = req.body;
 
       const estado_inicial = await getEstadoInicialTabla('facturas')
-      console.log("estado inicial", estado_inicial)
-      console.log("estado inicial", estado_inicial.id)
       const estado_movimiento = await estadoMovimientoModel.findOne({
         where: {
           [Op.and]: {
@@ -27,10 +25,8 @@ module.exports = {
         },
       })
       let total = 0;
-      console.log("estado final", estado_movimiento)
 
       const factura = await facturaModel.create({ ...cabecera, estado_factura_id: estado_movimiento.id })
-      console.log("factura")
       const factura_detalle = await Promise.all(detalle.map(
         async (det) => {
           total += det.precio
@@ -39,7 +35,6 @@ module.exports = {
           return factDet
         })
       )
-      console.log("detalle")
 
       await deudaModel.create({
         factura_id: factura.id,
@@ -49,7 +44,6 @@ module.exports = {
         debe: total,
         haber: 0
       })
-      console.log("deuda")
       const retornar = { factura, factura_detalle }
 
       return res.status(200).json({ mensaje: "Factura creada con éxito.", datos: retornar })
